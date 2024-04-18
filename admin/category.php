1285f203955486561fc9c7dd01af5d0de8639a1a
Sohil Vora <sohilVora2000@gmail.com>
2024-04-18 10:56:54 +0530

    update

--- admin/category.php
+++ admin/category.php
@@ -4,25 +4,29 @@
     <section class="category-section">
         <h1 class="text-uppercase border-bottom">category</h1>
         <button class="btn btn-primary add_category">Add New Category</button>
-        <div class="modal fade" id="catModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="catModal" tabindex="-1" aria-labelledby="modalTitleId" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <form action="" method="POST" id="cat_form">
+                    <form method="post" id="cat_form">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalTitleId"></h5>
+                            <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
                             <div class="form-group">
                                 <label for="category_name">Category Name</label>
-                                <input type="text" class="form-control" name="category_name" id="category_name ">
+                                <input type="text" name="category_name" class="form-control shadow-none" id="category_name">
+                                <span id="error" class="text-danger">
+                                </span>
                             </div>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary save">Save changes</button>
+                            <input type="hidden" name="category_id" id="category_id">
+                            <input type="hidden" name="form_type" id="form_type">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                Close
+                            </button>
+                            <button type="submit" class="btn btn-primary save" id="submit">Save</button>
                         </div>
                     </form>
                 </div>
@@ -32,21 +36,30 @@
 </div>
 <?php require_once "includes/footer.php"; ?>
 <script>
-    $(document).ready(function(){
-        $(document).on('submit','#cat_form',function(e){
+    $(document).ready(function() {
+        $(document).on('submit', '#cat_form', function(e) {
             e.preventDefault();
-           var fd = new FormData(this);
-           $.ajax({
-            url:'insert/cat_insert.php',
-            type:'POST',
-            data:fd,
-            dataTypr:'json',
-            processData:false,
-            contentType:false,
-            success: function(response){
-                console.log(response);
-            }
-           })
+            var fd = new FormData(this);
+            $.ajax({
+                url: 'insert/cat_insert.php',
+                type: 'POST',
+                data: fd,
+                dataType: 'json',
+                processData: false,
+                contentType: false,
+                success: function(response) {
+                    
+                    if (response.status == 0) {
+                        $("#error").html(response.msg_error);
+                    }
+                    if (response.status == 1) {
+                        $("#cat_form")[0].reset();
+                        $("#catModal").modal('hide');
+                        $("#error").html('');
+                        location.reload();
+                    }
+                }
+            });
         });
     });
 </script>
--- admin/insert/cat_insert.php
+++ admin/insert/cat_insert.php
@@ -2,15 +2,19 @@
 require_once '../../class/Crud.php';
 
 $obj = new Crud();
-
-$data  = [
-    'category_name' => $_POST['category_name'],
-    'category_slug_url' => $obj->slugify($_POST['category_name'], 'category_slug_url', 'category'),
-];
-$exe =  $obj->insert('category', $data);
-
-if ($exe = 1) {
-    echo 'success';
+if (empty($_POST['category_name'])) {
+    $data['msg_error'] = "Category name is required";
+    $data['status'] = 0;
 } else {
-    echo "fail";
+    $data  = [
+        'category_name' => $_POST['category_name'],
+        'category_slug_url' => $obj->slugify($_POST['category_name'], 'category_slug_url', 'category'),
+    ];
+    $exec =  $obj->insert('category', $data);
+    if ($exec = 1) {
+        $data['status'] = 1;
+    } else {
+        $data['status'] = 0;
+    }
 }
+echo json_encode($data);
